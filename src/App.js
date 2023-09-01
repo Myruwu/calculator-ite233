c8d44d48d4314633113f4c31099b47c36a4bad0a
Myro <myde.mercado.up@phinmaed.com>
2023-09-01 23:01:16 +0800

    changed the heading of the app

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Button from './components/Button/Button';
 function App() {
   return (
     <div className="app-container">
-      <h1 className="app-heading">Simple Calculator</h1>
+      <h1 className="app-heading">ITE 233 Calculator P2 Activity</h1>
       <Calculator />
     </div>
   );
